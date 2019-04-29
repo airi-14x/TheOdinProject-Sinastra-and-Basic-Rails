4c7ad9911967745afb0369873453f248548c57d0
airi-14x <airi.14x@gmail.com>
2019-04-29 16:28:56 -0400

    Delete Articles files

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,5 +1,27 @@
 class ArticlesController < ApplicationController
+include ArticlesHelper
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(
+      article_params
+    )
+    #@article.title = params[:article][:title]
+    #@article.body = params[:article][:body]
+    @article.save
+    redirect_to articles_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path(@article)
+  end
+
 end
