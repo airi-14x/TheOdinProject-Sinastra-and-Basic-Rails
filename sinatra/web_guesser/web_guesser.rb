b7c2248caa676086747ec29c28410a23d19165d5
airi-14x <airi.14x@gmail.com>
2019-05-21 15:21:31 -0400

    Functional Guessing code. Issue with Removing the Answer

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -1,7 +1,30 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
   current_number = Random.new(123).rand(100)
-  erb :index, :locals => {:current_number => current_number}
+  guess = params['guess']
+  message = ''
+  unless params['guess'].nil?
+    if Integer(guess) > current_number
+      message = if Integer(guess) > current_number + 5
+                  'Way Too High!'
+                else
+                  'Too High!'
+                end
+    end
+    if Integer(guess) < current_number
+      message = if Integer(guess) < current_number - 5
+                  'Way Too Low!'
+                else
+                  'Too Low!'
+                end
+    end
+    if Integer(guess) == current_number
+      message = 'Perfect! Congrats, your guess was correct!'
+    end
+    erb :index, locals: { current_number: current_number, message: message }
+end
 end
