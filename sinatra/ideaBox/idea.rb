8467ad60fff7b860edccdbe114aa0826241a4968
airi-14x <airi.14x@gmail.com>
2019-06-17 13:10:59 -0400

    Check YAML file

--- sinatra/ideaBox/idea.rb
+++ sinatra/ideaBox/idea.rb
@@ -1,7 +1,16 @@
 # Business Logic about an Idea
+require 'yaml/store'
 
 class Idea
   def save
+    database.transaction do |db|
+      db['ideas'] ||= []
+      db['ideas'] << {title: 'diet', description: 'pizza all the time'}
+    end
+  end
+
+  def database
+    @database ||= YAML::Store.new "ideabox"
   end
 
 end
