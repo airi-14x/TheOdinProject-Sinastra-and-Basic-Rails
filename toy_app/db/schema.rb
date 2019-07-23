ba50af5e06b4345004fae29db3213cb7be9e1540
airi-14x <airi.14x@gmail.com>
2019-07-23 14:57:09 -0400

    Complete toy_app

--- toy_app/db/schema.rb
+++ toy_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_142021) do
+ActiveRecord::Schema.define(version: 2019_07_23_141954) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
